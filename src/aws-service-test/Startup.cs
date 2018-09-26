5b20316fae1119060cd993a20a6bfa6eb186a5b3
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2018-09-26 21:13:52 +0100

    Added AWS SQS subscription (#3)
    * Added initial implementation of receiving message from an SQS and pushing them down a websocket channel
    
    * Updates to remove hard coded values in C# codebase

--- src/aws-service-test/Startup.cs
+++ src/aws-service-test/Startup.cs
@@ -1,19 +1,26 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+
 using Nancy.Owin;
+using Amazon.SQS;
+
 using aws_service_test.WebSockets;
 using aws_service_test.Connections;
+using aws_service_test.Processors;
+using aws_service_test.Handlers;
+using Amazon.SQS.Model;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace aws_service_test
 {
     public class Startup
     {
+        private const string kQueueNamePrefix = "aws-service-test-queue";
         private const uint kWebSocketKeepAliveIntervale = 30;
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -29,13 +36,34 @@ namespace aws_service_test
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            /*var lifetime = app.ApplicationServices.GetRequiredService<IApplicationLifetime>();
+            var lifetime = app.ApplicationServices.GetRequiredService<IApplicationLifetime>();
+
+            IDisposable processor = null;
+
+            lifetime.ApplicationStarted.Register(() =>
+            {
+                var config = new AmazonSQSConfig();
+                config.ServiceURL = Environment.ExpandEnvironmentVariables("%SQSENDPOINT%");
+
+                var client = new AmazonSQSClient(config);
+
+                var request = new ListQueuesRequest();
+                request.QueueNamePrefix = kQueueNamePrefix;
+
+                var queues = client.ListQueuesAsync(request).Result;
+                var sqsUrl = queues.QueueUrls.FirstOrDefault();
+
+                var manager = app.ApplicationServices.GetService<IConnectionManager>();
+                var handler = new SqsMessageHandler(manager);
+
+                processor = new SqsProcessor(client, sqsUrl, handler);
+            });
+
 
             lifetime.ApplicationStopped.Register(() =>
             {
-                var manager = app.ApplicationServices.GetService<IConnectionManager>();
-                manager.Dispose();
-            });*/
+                processor?.Dispose();
+            });
 
             if (env.IsDevelopment())
             {
