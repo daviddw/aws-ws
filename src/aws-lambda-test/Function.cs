87a61af695821695f5151691fd2362080408b699
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2018-09-28 07:43:38 +0100

    Added inital implementation of lambda stack and function

---
+++ src/aws-lambda-test/Function.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace aws_lambda_test
+{
+    public class Function
+    {
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input?.ToUpper();
+        }
+    }
+}
