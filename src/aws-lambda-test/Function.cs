108887e07f708500cae711616675f43a2af5c21c
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2018-09-30 08:42:25 +0100

    Updates to lambda to pass POST body to SQS

--- src/aws-lambda-test/Function.cs
+++ src/aws-lambda-test/Function.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -8,9 +13,37 @@ namespace aws_lambda_test
 {
     public class Function
     {
-        public string FunctionHandler(string input, ILambdaContext context)
+        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            context.Logger.LogLine(">FunctionHandler\n");
+
+            var config = new AmazonSQSConfig();
+            config.ServiceURL = Environment.ExpandEnvironmentVariables("%SQSENDPOINT%");
+
+            var client = new AmazonSQSClient(config);
+
+            context.Logger.LogLine("AmazonSQSClient created\n");
+
+            var request = new SendMessageRequest
+            {
+                QueueUrl = Environment.ExpandEnvironmentVariables("%QUEUENAME%"),
+                MessageBody = input.Body,
+            };
+
+            client.SendMessageAsync(request).Wait();
+
+            context.Logger.LogLine("SendMessageAsync completed\n");
+
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = input.Body,
+                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } },
+            };
+
+            context.Logger.LogLine("<FunctionHandler\n");
+
+            return response;
         }
     }
 }
