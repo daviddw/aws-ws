12cec0ffc0c62310ace8879d418617fe6127a4d7
David Drysdale-Wilson <david.drysdale-wilson@linn.co.uk>
2018-09-30 22:31:05 +0100

    Add lambda (#4)
    * Create new build targets and created a variable to define stack name
    
    * Added inital implementation of lambda stack and function
    
    * Added lambda project to solution
    
    * Added environment variable QUEUENAME, so lambda function can get access queue
    
    * Factored out building of ws service and lambda function
    
    * Partial change to hook lambda up to API gateway
    
    * Set build date/time and renamed some build targets
    
    * Fixed some build script errors
    
    * Fixed docker tag
    
    * Fixed README.md to match build target changes
    
    * Added newline
    
    * Updates to lambda to pass POST body to SQS
    
    * Updates to lambda cloudformation file to correctly configure lambda
    
    * Updated build script to allow compilation of lambda and pushing to s3 as well as lambda stack starting/stopping
    
    * Changed DeleteStacks to StopStacks to match StartStacks
    
    * Deploy -> PushDocker
    
    * Added back commented out stack deployments
    
    * Changed lambda to publish message to SNS topic rather than directly into SQS queue

--- fake.fsx
+++ fake.fsx
@@ -1,5 +1,6 @@
 #r "paket:
 nuget Fake.IO.FileSystem
+nuget Fake.IO.Zip
 nuget Fake.DotNet.Cli
 nuget Fake.Core.Process
 nuget Fake.Core.Target //"
@@ -12,24 +13,37 @@ open Fake.IO
 open Fake.DotNet
 open Fake.IO.Globbing.Operators
 
+let date = DateTime.Now.ToString "yyyy-MM-ddTHH:mm:ssZ"
+
 // Properties
 let projectName = "aws-service-test"
 
 let vcsRef = Environment.environVarOrDefault "VCSREF" ""
-let buildDate = Environment.environVarOrDefault "BUILDDATE" ""
+let vcsBranch = Environment.environVarOrDefault "VCSBRANCH" ""
+let buildDate = Environment.environVarOrDefault "BUILDDATE" date
 let configuration = Environment.environVarOrDefault "CONFIGURATION" ""
 
 let baseDir = __SOURCE_DIRECTORY__
 let sourceDir = Path.combine baseDir "src"
-let solutionDir = sourceDir
-let projectDir = Path.combine sourceDir projectName |> Path.GetFullPath
-let buildDir = Path.combine projectDir (Path.combine "bin" configuration) |> Path.GetFullPath
-let deployDir = Path.combine projectDir (Path.combine "deploy" configuration) |> Path.GetFullPath
+
+let serviceDir = Path.combine sourceDir "aws-service-test"
+let serviceBuildDir = Path.combine serviceDir (Path.combine "bin" configuration) |> Path.GetFullPath
+let serviceDeployDir = Path.combine serviceDir (Path.combine "deploy" configuration) |> Path.GetFullPath
+
+let lambdaDir = Path.combine sourceDir "aws-lambda-test"
+let lambdaBuildDir = Path.combine lambdaDir (Path.combine "bin" configuration) |> Path.GetFullPath
+let lambdaDeployDir = Path.combine lambdaDir (Path.combine "deploy" configuration) |> Path.GetFullPath
+let lambdaPackageDir = Path.combine lambdaDir "deploy" |> Path.GetFullPath
+let lambdaPackageFilename = "aws-lambda-test.zip"
 
 let dockerUser = Environment.environVarOrDefault "DOCKERUSER" ""
-let tag = Environment.environVarOrDefault "DOCKERTAG" "" |> fun s -> s.Replace('/', '-')
+let tag = vcsRef + "-" + vcsBranch |> fun s -> s.Replace('/', '-')
 
 let dockerImage = dockerUser + "/" + projectName + ":" + tag
+
+let bucketName = Environment.environVarOrDefault "S3BUCKET" ""
+
+let stackName = "aws-service-test"
 
 let awsDir = Path.combine baseDir "aws"
 let sshKey = Environment.environVarOrDefault "AWSKEYNAME" ""
@@ -40,17 +54,24 @@ let subnets = Environment.environVarOrDefault "AWSSUBNETS" ""
 // Targets
 Target.create "Clean" (fun _ ->
     Shell.cleanDirs
-        [ deployDir
-          buildDir
-          deployDir ]
+        [ serviceBuildDir
+          serviceDeployDir
+          lambdaBuildDir
+          lambdaDeployDir ]
 )
 
 Target.create "Build" (fun _ ->
     DotNet.build (fun c ->
         { c with
             Configuration = DotNet.BuildConfiguration.Custom(configuration)
-            OutputPath = Some(buildDir)
-        }) solutionDir
+            OutputPath = Some(serviceBuildDir)
+        }) serviceDir
+
+    DotNet.build (fun c ->
+        { c with
+            Configuration = DotNet.BuildConfiguration.Custom(configuration)
+            OutputPath = Some(lambdaBuildDir)
+        }) lambdaDir
 )
 
 Target.create "Publish" (fun _ ->
@@ -61,28 +82,53 @@ Target.create "Publish" (fun _ ->
                     CustomParams = Some("/property:PublishWithAspNetCoreTargetManifest=false")
                 })
             Configuration = DotNet.BuildConfiguration.Release;
-            OutputPath = Some(deployDir)
-        }) solutionDir
+            OutputPath = Some(serviceDeployDir)
+        }) serviceDir
+
+    DotNet.publish (fun c ->
+        { c with
+            Common = DotNet.Options.Create() |> (fun c ->
+                { c with
+                    CustomParams = Some("/property:PublishWithAspNetCoreTargetManifest=false")
+                })
+            Configuration = DotNet.BuildConfiguration.Release;
+            OutputPath = Some(lambdaDeployDir)
+        }) lambdaDir
+
+    let files = Directory.EnumerateFiles(lambdaDeployDir)
+    Fake.IO.Zip.zip lambdaDeployDir (lambdaPackageDir + "/" + lambdaPackageFilename) files
 
     ignore(Shell.Exec("docker", "build -f Dockerfile -t " + dockerImage + " --build-arg VCSREF=" + vcsRef + " --build-arg VERSION=" + tag + " --build-arg BUILDDATE=" + buildDate + " .", sourceDir))
+)
 
+Target.create "PushLambda" (fun _ ->
+    ignore(Shell.Exec("aws", "s3 cp " + lambdaPackageFilename + " s3://" + bucketName, lambdaPackageDir))
+)
+
+Target.create "PushDocker" (fun _ ->
     ignore(Shell.Exec("docker", "push " + dockerUser + "/" + projectName, sourceDir))
 )
 
-Target.create "Deploy" (fun _ ->
-    ignore(Shell.Exec("aws", "cloudformation deploy --stack-name " + projectName + "-queue --template-file sqs.yaml", awsDir))
+Target.create "StartStacks" (fun _ ->
+    ignore(Shell.Exec("aws", "cloudformation deploy --stack-name " + stackName + "-queue --template-file sqs.yaml", awsDir))
 
-    ignore(Shell.Exec("aws", "cloudformation deploy --stack-name " + projectName + "-ecs --template-file ecs.yaml --capabilities CAPABILITY_IAM --parameter-overrides KeyName=" + sshKey + " VpcId=" + vpcId + " SubnetIds=" + subnets + " DockerImage=" + dockerImage + " DockerTag=" + tag, awsDir))
+    ignore(Shell.Exec("aws", "cloudformation deploy --stack-name " + stackName + "-lambda --template-file lambda.yaml --capabilities CAPABILITY_IAM --parameter-overrides BucketName=" + bucketName + " LambdaPackage=" + lambdaPackageFilename, awsDir))
+
+    ignore(Shell.Exec("aws", "cloudformation deploy --stack-name " + stackName + "-ecs --template-file ecs.yaml --capabilities CAPABILITY_IAM --parameter-overrides KeyName=" + sshKey + " VpcId=" + vpcId + " SubnetIds=" + subnets + " DockerImage=" + dockerImage + " DockerTag=" + tag, awsDir))
 )
 
-Target.create "DeleteStack" (fun _ ->
-    ignore(Shell.Exec("aws", "cloudformation delete-stack --stack-name " + projectName + "-ecs", awsDir))
+Target.create "StopStacks" (fun _ ->
+    ignore(Shell.Exec("aws", "cloudformation delete-stack --stack-name " + stackName + "-ecs", awsDir))
 
-    ignore(Shell.Exec("aws", "cloudformation wait stack-delete-complete --stack-name "+ projectName + "-ecs", awsDir))
+    ignore(Shell.Exec("aws", "cloudformation wait stack-delete-complete --stack-name "+ stackName + "-ecs", awsDir))
 
-    ignore(Shell.Exec("aws", "cloudformation delete-stack --stack-name " + projectName + "-queue", awsDir))
+    ignore(Shell.Exec("aws", "cloudformation delete-stack --stack-name " + stackName + "-lambda", awsDir))
 
-    ignore(Shell.Exec("aws", "cloudformation wait stack-delete-complete --stack-name "+ projectName + "-queue", awsDir))
+    ignore(Shell.Exec("aws", "cloudformation wait stack-delete-complete --stack-name "+ stackName + "-lambda", awsDir))
+
+    ignore(Shell.Exec("aws", "cloudformation delete-stack --stack-name " + stackName + "-queue", awsDir))
+
+    ignore(Shell.Exec("aws", "cloudformation wait stack-delete-complete --stack-name "+ stackName + "-queue", awsDir))
 )
 
 open Fake.Core.TargetOperators
@@ -92,5 +138,9 @@ open Fake.Core.TargetOperators
     ==> "Build"
     ==> "Publish"
 
+"Publish"
+    ==> "PushLambda"
+    ==> "PushDocker"
+
 // start build
-Target.runOrDefaultWithArguments "Build"
+Target.runOrDefaultWithArguments "PushDocker"
---
+++ src/aws-lambda-test/Function.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace aws_lambda_test
+{
+    public class Function
+    {
+        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
+        {
+            context.Logger.LogLine(">FunctionHandler\n");
+
+            var config = new AmazonSimpleNotificationServiceConfig();
+            config.ServiceURL = Environment.ExpandEnvironmentVariables("%SNSENDPOINT%");
+
+            var client = new AmazonSimpleNotificationServiceClient(config);
+
+            context.Logger.LogLine("AmazonSNSClient created\n");
+
+            var request = new PublishRequest
+            {
+                TopicArn = Environment.ExpandEnvironmentVariables("%TOPICARN%"),
+                Message = input.Body,
+            };
+
+            client.PublishAsync(request).Wait();
+
+            context.Logger.LogLine("PublishAsync completed\n");
+
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = input.Body,
+                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } },
+            };
+
+            context.Logger.LogLine("<FunctionHandler\n");
+
+            return response;
+        }
+    }
+}
